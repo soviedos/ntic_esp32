b62ca24e9b54e5a4c097e32234714cf7f1f0c55c
Sergio Oviedo <soviedos@gmail.com>
2021-03-07 20:59:23 -0600

    First iteration of sensor MAX30102, SPO2 and temperature is good

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,69 @@
-#include <Arduino.h>
 
+/*
+  ## Hardware Connections (ESP32 Arduino):
+  -VIN = 3.3V
+  -GND = GND
+  -SDA = 21 (or SDA)
+  -SCL = 22 (or SCL)
+
+*/
+
+// Libraries required
+#include <Arduino.h>
+#include <Wire.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include "MAX30105.h" //sparkfun MAX3010X library
+#include "heartRate.h"
+#include "credentials.h"
 
-#define IOT_HUB_NAME "NTicSolution"
-#define DEVICE_NAME "esp32"
-#define SAS_TOKEN "SharedAccessSignature sr=NTicSolution.azure-devices.net%2Fdevices%2Fesp32&sig=Efmz0F7r9zocWE97Y4IYMDdEuLw2tl6sJUN9TE0eTxo%3D&se=1648039565"
+// Azure IoT Hub Credentials
+#define IOT_HUB_NAME IOT_HUB_NAME_NTic
+#define DEVICE_NAME DEVICE_NAME_NTic
+#define SAS_TOKEN SAS_TOKEN_NTic
 
-const char* ssid     = "TNT3";     // your network SSID (name of wifi network)
-const char* password = "DFGHMNBVZX"; // your network password
+// WiFi Credentials
+const char* ssid     = SSID_SECRET;     // your network SSID (name of wifi network)
+const char* password = PASS_SECRET; // your network password
 
-// Created with openssl s_client -showcerts -connect thingpulse.azure-devices.net:443
-// and by picking the root certificate
-const char* root_ca= \
-     "-----BEGIN CERTIFICATE-----\n" \
-"MIIFWjCCBEKgAwIBAgIQD6dHIsU9iMgPWJ77H51KOjANBgkqhkiG9w0BAQsFADBa\n" \
-"MQswCQYDVQQGEwJJRTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJl\n" \
-"clRydXN0MSIwIAYDVQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTIw\n" \
-"MDcyMTIzMDAwMFoXDTI0MTAwODA3MDAwMFowTzELMAkGA1UEBhMCVVMxHjAcBgNV\n" \
-"BAoTFU1pY3Jvc29mdCBDb3Jwb3JhdGlvbjEgMB4GA1UEAxMXTWljcm9zb2Z0IFJT\n" \
-"QSBUTFMgQ0EgMDIwggIiMA0GCSqGSIb3DQEBAQUAA4ICDwAwggIKAoICAQD0wBlZ\n" \
-"qiokfAYhMdHuEvWBapTj9tFKL+NdsS4pFDi8zJVdKQfR+F039CDXtD9YOnqS7o88\n" \
-"+isKcgOeQNTri472mPnn8N3vPCX0bDOEVk+nkZNIBA3zApvGGg/40Thv78kAlxib\n" \
-"MipsKahdbuoHByOB4ZlYotcBhf/ObUf65kCRfXMRQqOKWkZLkilPPn3zkYM5GHxe\n" \
-"I4MNZ1SoKBEoHa2E/uDwBQVxadY4SRZWFxMd7ARyI4Cz1ik4N2Z6ALD3MfjAgEED\n" \
-"woknyw9TGvr4PubAZdqU511zNLBoavar2OAVTl0Tddj+RAhbnX1/zypqk+ifv+d3\n" \
-"CgiDa8Mbvo1u2Q8nuUBrKVUmR6EjkV/dDrIsUaU643v/Wp/uE7xLDdhC5rplK9si\n" \
-"NlYohMTMKLAkjxVeWBWbQj7REickISpc+yowi3yUrO5lCgNAKrCNYw+wAfAvhFkO\n" \
-"eqPm6kP41IHVXVtGNC/UogcdiKUiR/N59IfYB+o2v54GMW+ubSC3BohLFbho/oZZ\n" \
-"5XyulIZK75pwTHmauCIeE5clU9ivpLwPTx9b0Vno9+ApElrFgdY0/YKZ46GfjOC9\n" \
-"ta4G25VJ1WKsMmWLtzyrfgwbYopquZd724fFdpvsxfIvMG5m3VFkThOqzsOttDcU\n" \
-"fyMTqM2pan4txG58uxNJ0MjR03UCEULRU+qMnwIDAQABo4IBJTCCASEwHQYDVR0O\n" \
-"BBYEFP8vf+EG9DjzLe0ljZjC/g72bPz6MB8GA1UdIwQYMBaAFOWdWTCCR1jMrPoI\n" \
-"VDaGezq1BE3wMA4GA1UdDwEB/wQEAwIBhjAdBgNVHSUEFjAUBggrBgEFBQcDAQYI\n" \
-"KwYBBQUHAwIwEgYDVR0TAQH/BAgwBgEB/wIBADA0BggrBgEFBQcBAQQoMCYwJAYI\n" \
-"KwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRpZ2ljZXJ0LmNvbTA6BgNVHR8EMzAxMC+g\n" \
-"LaArhilodHRwOi8vY3JsMy5kaWdpY2VydC5jb20vT21uaXJvb3QyMDI1LmNybDAq\n" \
-"BgNVHSAEIzAhMAgGBmeBDAECATAIBgZngQwBAgIwCwYJKwYBBAGCNyoBMA0GCSqG\n" \
-"SIb3DQEBCwUAA4IBAQCg2d165dQ1tHS0IN83uOi4S5heLhsx+zXIOwtxnvwCWdOJ\n" \
-"3wFLQaFDcgaMtN79UjMIFVIUedDZBsvalKnx+6l2tM/VH4YAyNPx+u1LFR0joPYp\n" \
-"QYLbNYkedkNuhRmEBesPqj4aDz68ZDI6fJ92sj2q18QvJUJ5Qz728AvtFOat+Ajg\n" \
-"K0PFqPYEAviUKr162NB1XZJxf6uyIjUlnG4UEdHfUqdhl0R84mMtrYINksTzQ2sH\n" \
-"YM8fEhqICtTlcRLr/FErUaPUe9648nziSnA0qKH7rUZqP/Ifmbo+WNZSZG1BbgOh\n" \
-"lk+521W+Ncih3HRbvRBE0LWYT8vWKnfjgZKxwHwJ\n" \
-"-----END CERTIFICATE-----\n";
-
-
+// WiFi  object instance
 WiFiClientSecure client;
 
+// Created with openssl s_client -showcerts -connect NTicSolutions.azure-devices.net:443
+// and by picking the root certificate
+const char* root_ca = ROOT_CERT; // Root Certificate for Azure IoT Hub
+
+// MAX30102 sensor instance
+MAX30105 particleSensor;
+
+//MAX30102 configuration parameters
+double avered = 0; double aveir = 0;
+double sumirrms = 0;
+double sumredrms = 0;
+int i = 0;
+int Num = 100;//calculate SpO2 by this sampling interval
+
+double ESpO2 = 95.0;//initial value of estimated SpO2
+double FSpO2 = 0.7; //filter factor for estimated SpO2
+double frate = 0.95; //low pass filter for IR/red LED value to eliminate AC component
+#define TIMETOBOOT 3000 // wait for this time(msec) to output SpO2
+#define SCALE 88.0 //adjust to display heart beat and SpO2 in the same scale
+#define SAMPLING 5 //if you want to see heart beat more precisely , set SAMPLING to 1
+#define FINGER_ON 30000 // if red signal is lower than this , it indicates your finger is not on the sensor
+#define MINIMUM_SPO2 80.0
+
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+float beatsPerMinute;
+int beatAvg;
+
+#define USEFIFO
+
 void sendRequest(String iothubName, String deviceName, String sasToken, String message) {
+
+  // HTTPS connection to Azure IoT Hub
   if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
 
     HTTPClient http;
@@ -87,9 +102,87 @@ void setup() {
     delay(1000);
   }
 
+  // MAX30102 sensor initialization
+  while (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30102 was not found ");
+  }
+
+  //Setup to sense a nice looking saw tooth on the plotter
+  byte ledBrightness = 0x7F; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  //Options: 1 = IR only, 2 = Red + IR on MH-ET LIVE MAX30102 board
+  int sampleRate = 200; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
+  // Set up the wanted parameters
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+
+  particleSensor.enableDIETEMPRDY();
+
 }
 
 void loop() {
   sendRequest(IOT_HUB_NAME, DEVICE_NAME, SAS_TOKEN, "{temperature: 24.5}");
   delay(10000);
+
+  uint32_t ir, red , green;
+  double fred, fir;
+  double SpO2 = 0; //raw SpO2 before low pass filtered
+
+#ifdef USEFIFO
+  particleSensor.check(); //Check the sensor, read up to 3 samples
+
+  while (particleSensor.available()) {//do we have new data
+#ifdef MAX30105
+   red = particleSensor.getFIFORed(); //Sparkfun's MAX30105
+    ir = particleSensor.getFIFOIR();  //Sparkfun's MAX30105
+#else
+    red = particleSensor.getFIFOIR(); //why getFOFOIR output Red data by MAX30102 on MH-ET LIVE breakout board
+    ir = particleSensor.getFIFORed(); //why getFIFORed output IR data by MAX30102 on MH-ET LIVE breakout board
+#endif
+
+    
+    
+    i++;
+    fred = (double)red;
+    fir = (double)ir;
+    avered = avered * frate + (double)red * (1.0 - frate);//average red level by low pass filter
+    aveir = aveir * frate + (double)ir * (1.0 - frate); //average IR level by low pass filter
+    sumredrms += (fred - avered) * (fred - avered); //square sum of alternate component of red level
+    sumirrms += (fir - aveir) * (fir - aveir);//square sum of alternate component of IR level
+    if ((i % SAMPLING) == 0) {//slow down graph plotting speed for arduino Serial plotter by thin out
+      if ( millis() > TIMETOBOOT) {
+        float ir_forGraph = (2.0 * fir - aveir) / aveir * SCALE;
+        float red_forGraph = (2.0 * fred - avered) / avered * SCALE;
+        //trancation for Serial plotter's autoscaling
+        if ( ir_forGraph > 100.0) ir_forGraph = 100.0;
+        if ( ir_forGraph < 80.0) ir_forGraph = 80.0;
+        if ( red_forGraph > 100.0 ) red_forGraph = 100.0;
+        if ( red_forGraph < 80.0 ) red_forGraph = 80.0;
+                Serial.print(red); Serial.print(","); Serial.print(ir);Serial.println(".");
+        if (ir < FINGER_ON) ESpO2 = MINIMUM_SPO2; //indicator for finger detached
+        float temperature = particleSensor.readTemperature();
+        Serial.print("Temperature C = ");
+        Serial.println(temperature);
+        //Blynk.run();
+        //Blynk.virtualWrite(V4,ESpO2 );
+        Serial.print("Oxygen % = ");
+        Serial.println(ESpO2);
+      }
+    }
+    if ((i % Num) == 0) {
+      double R = (sqrt(sumredrms) / avered) / (sqrt(sumirrms) / aveir);
+       Serial.println(R);
+      SpO2 = -23.3 * (R - 0.4) + 100; //http://ww1.microchip.com/downloads/jp/AppNotes/00001525B_JP.pdf
+      ESpO2 = FSpO2 * ESpO2 + (1.0 - FSpO2) * SpO2;//low pass filter
+        Serial.print(SpO2);Serial.print(",");Serial.println(ESpO2);
+      sumredrms = 0.0; sumirrms = 0.0; i = 0;
+      break;
+    }
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+    Serial.println(SpO2);
+  }
+#endif
 }
